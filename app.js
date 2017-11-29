8fcadbd3b576da747bce833f68387d9d62849226
Ruslan Timurziyev <r.timurziyev@kagis.kz>
2017-11-29 17:39:09 +0600

    +

--- app.js
+++ app.js
@@ -5,9 +5,7 @@
 
   function addProp (path, key, val, hist, idx=0) {
     if (idx < path.length) {
-      if (!hist.hasOwnProperty(path[idx])) {
-        hist[path[idx]] = {}; 
-      }
+      if (!hist.hasOwnProperty(path[idx])) hist[path[idx]] = {};
       addProp(path, key, val, hist[path[idx]], ++idx)
     } else {
       hist[key] = val;
@@ -16,25 +14,16 @@
   }
 
   function pushItem (path, item, hist, idx=0) {
-    if (idx < path.length) {
-      pushItem(path, item, hist[path[idx]], ++idx)
-    } else {
-      hist.push(item);
-    }
+    if (idx < path.length) pushItem(path, item, hist[path[idx]], ++idx)
+    else hist.push(item);
     return hist;
   }
   
 
   context.App.Module = {
-    addProp (path, key, val) {
-      return addProp(path, key, val, store);
-    },
-    pushItem (path, item) {
-      return pushItem (path, item, store)
-    },
-    getAll () {
-      return store;
-    }
+    addProp: (path, key, val) => addProp(path, key, val, store),
+    pushItem: (path, item) => pushItem (path, item, store),
+    getAll: () => store,
   }
 
 })(this);
@@ -44,14 +33,10 @@
 
 
 App.Module.addProp([1, 2, 3], 'key3', 'val');
-
 App.Module.addProp([1, 2, 3, 4], 'key4', 'val');
 App.Module.addProp([1, 2, 3, 4], 'key4_1', 'val');
 App.Module.addProp([1, 2, 3, 4], 'key4_2', 'val');
-
 App.Module.addProp([1, 2], 'key2', []);
-
-
 
 App.Module.pushItem([1, 2, 'key2'], {
   name: 'sow',
